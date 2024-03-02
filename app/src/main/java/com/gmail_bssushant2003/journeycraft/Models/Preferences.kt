f6c2fa150afee40014c67d2eb0ac4d391116a858
Gaurav <gauravbodake1@gmail.com>
2024-03-02 13:12:58 +0530

    added tick in preferenceActivity

--- app/src/main/java/com/gmail_bssushant2003/journeycraft/Models/Preferences.kt
+++ app/src/main/java/com/gmail_bssushant2003/journeycraft/Models/Preferences.kt
@@ -3,12 +3,14 @@ package com.gmail_bssushant2003.journeycraft.Models
 import java.io.Serializable
 
 data class Preferences(
-    private val isLiftedTemple : Boolean,
-    private var isLiftedBeach : Boolean,
-    private var isLiftedHistorical : Boolean,
-    private var isLiftedMountain : Boolean,
-    private var isLiftedLake : Boolean,
-    private var isLiftedMuseum : Boolean,
-    private var isLiftedPark : Boolean,
-    private var isLiftedWildlife : Boolean
+    val isLiftedTemple : Boolean,
+    var isLiftedBeach : Boolean,
+    var isLiftedHistorical : Boolean,
+    var isLiftedMountain : Boolean,
+    var isLiftedLake : Boolean,
+    var isLiftedMuseum : Boolean,
+    var isLiftedPark : Boolean,
+    var isLiftedWildlife : Boolean
 ) : Serializable
+
+
