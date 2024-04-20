f23a69ec64a6c98fff97fb569cc1a8326688619c
Gaurav <gauravbodake1@gmail.com>
2024-04-20 23:21:00 +0530

    Weather card added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,8 +49,23 @@ dependencies {
     implementation("com.google.maps:google-maps-services:0.14.0")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation ("com.google.android.gms:play-services-location:21.2.0")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation ("com.github.dangiashish:Google-Direction-Api:1.6")
+
+
+    //weather
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation("com.google.code.gson:gson:2.9.1")
+    implementation("com.github.MatteoBattilana:WeatherView:3.0.0")
+    implementation("androidx.activity:activity-ktx:1.8.2")
+    implementation("com.github.Dimezis:BlurView:version-2.0.3")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
 
 
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -58,6 +73,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.drawerlayout:drawerlayout:1.2.0")
     implementation("com.google.android.libraries.places:places:3.4.0")
+    implementation("androidx.activity:activity:1.8.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/gmail_bssushant2003/journeycraft/MainActivity.kt
+++ app/src/main/java/com/gmail_bssushant2003/journeycraft/MainActivity.kt
@@ -1,26 +1,28 @@
 package com.gmail_bssushant2003.journeycraft
 
 import android.content.Intent
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
-import androidx.cardview.widget.CardView
-import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AppCompatActivity
 import com.gmail_bssushant2003.journeycraft.IntercityTransport.IntercityTransportActivity
-import com.gmail_bssushant2003.journeycraft.Models.Items
 import com.gmail_bssushant2003.journeycraft.Transport.TransportActivity
+import com.gmail_bssushant2003.journeycraft.Weather.Activity.WeatherMainActivity
 import com.gmail_bssushant2003.journeycraft.databinding.ActivityMainBinding
 import com.google.maps.model.LatLng
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
-    private var isLiftedVeg = false
-    private var isLiftedNonVeg = false
-    private lateinit var backBtn: ImageView
+
+    private val latLngMap : Map<String, LatLng> = mapOf(
+        "Kolhapur" to LatLng(16.691307, 74.244865),
+        "Goa" to LatLng(15.496777, 73.827827),
+        "Ooty" to LatLng(11.410000,76.699997),
+        "Mahabaleshwar" to LatLng(17.921721, 73.655602),
+        "Lakshadweep" to LatLng(11.7056501,72.7152889),
+        "Manali" to LatLng(32.239632,77.188713)
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,9 +37,6 @@ class MainActivity : AppCompatActivity() {
         val individualDestination = intent.getStringExtra("individualDestination")
         val destinationstate = intent.getStringExtra("statedestination")
 
-//        Toast.makeText(this,individualDestination,Toast.LENGTH_SHORT).show()
-//        Toast.makeText(this,destinationstate,Toast.LENGTH_LONG).show()
-
         val textViewindidest = findViewById<TextView>(R.id.individualdest)
         val textViewstate = findViewById<TextView>(R.id.statedest)
 
@@ -49,36 +48,46 @@ class MainActivity : AppCompatActivity() {
             onBackPressed()
         }
 
-        if(individualDestination == "Kolhapur"){
-            latLng = LatLng(16.691307, 74.244865)
-        }
-        else if(individualDestination == "Goa"){
-            latLng = LatLng(15.496777, 73.827827)
-        }
-        else if(individualDestination == "Ooty"){
-            latLng = LatLng(11.410000,76.699997)
-        }
-        else if(individualDestination == "Mahabaleshwar"){
-            latLng = LatLng(17.921721, 73.655602)
-        }
-        else if(individualDestination == "Lakshadweep"){
-            latLng = LatLng(11.7056501,72.7152889)
-        }
-        else if(individualDestination == "Manali"){
-            latLng = LatLng(32.239632,77.188713)
-        }
+//        if(individualDestination == "Kolhapur"){
+//            latLng = LatLng(16.691307, 74.244865)
+//        }
+//        else if(individualDestination == "Goa"){
+//            latLng = LatLng(15.496777, 73.827827)
+//        }
+//        else if(individualDestination == "Ooty"){
+//            latLng = LatLng(11.410000,76.699997)
+//        }
+//        else if(individualDestination == "Mahabaleshwar"){
+//            latLng = LatLng(17.921721, 73.655602)
+//        }
+//        else if(individualDestination == "Lakshadweep"){
+//            latLng = LatLng(11.7056501,72.7152889)
+//        }
+//        else if(individualDestination == "Manali"){
+//            latLng = LatLng(32.239632,77.188713)
+//        }
 
+        //on click available transport
         binding.availableTransport.setOnClickListener {
             val intent = Intent(this, TransportActivity::class.java)
-            intent.putExtra("destinationLat", latLng.lat)
-            intent.putExtra("destinationLng", latLng.lng)
+            intent.putExtra("destinationLat", latLngMap[individualDestination]!!.lat)
+            intent.putExtra("destinationLng", latLngMap[individualDestination]!!.lng)
             startActivity(intent)
         }
 
+        //on click intercity transport
         binding.intercityTransport.setOnClickListener {
             val intent = Intent(this, IntercityTransportActivity::class.java)
             startActivity(intent)
         }
 
+        //on click weather card
+        binding.weatherCard.setOnClickListener {
+            val intent = Intent(this, WeatherMainActivity::class.java)
+            intent.putExtra("lat", latLngMap[individualDestination]!!.lat)
+            intent.putExtra("lon", latLngMap[individualDestination]!!.lng)
+            intent.putExtra("name", individualDestination)
+            startActivity(intent)
+        }
     }
 }
