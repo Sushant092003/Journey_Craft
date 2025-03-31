d1717708f9859c7db0e4884e3c329fdbd65ad1e7
Sushant <bssushant2003@gmail.com>
2025-03-31 14:25:37 +0530

    Minor Changes

---
+++ app/src/main/java/com/gmail_bssushant2003/journeycraft/Models/Restaurant.kt
@@ -0,0 +1,22 @@
+package com.gmail_bssushant2003.journeycraft.Models
+
+import java.time.LocalTime
+
+data class Restaurant(
+    val id: Long? = null,
+    val user: User? = null,
+    val name: String? = null,
+    val rating: Double? = null,
+    val locationLink: String? = null,
+    val fssaiLicense: String? = null,
+    val openTime: String? = null,
+    val closeTime: String? = null,
+    val description: String? = null,
+    val phoneNo: String? = null,
+    val averageCost: Double? = null,
+    val foodType: FoodType? = null
+) {
+    enum class FoodType {
+        VEG, NON_VEG
+    }
+}
